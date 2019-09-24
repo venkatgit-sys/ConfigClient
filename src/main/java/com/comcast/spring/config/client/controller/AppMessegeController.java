5a78270266b2ff28603bcc4d50085a40bd374025
588520 <588520@PC368278.cts.com>
2019-09-24 11:53:09 +0530

    controller Refresh Annotation added

--- src/main/java/com/comcast/spring/config/client/controller/AppMessegeController.java
+++ src/main/java/com/comcast/spring/config/client/controller/AppMessegeController.java
@@ -1,10 +1,11 @@
 package com.comcast.spring.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RefreshScope
 @RestController
 @RequestMapping("/app")
 public class AppMessegeController {
